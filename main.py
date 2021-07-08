eeb9e5c70ffce11e0f3b3e70fa231268c1f34e67
SCJackB <broughjv@scotscollege.school.nz>
2021-07-08 12:27:01 +1200

    Added comments to main.py and __init__.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#improt flask app function from __init__
 from website import create_app
 
+#define 'app' as the flask app without running it
 app = create_app()
 
+#run the flask app w/ debugging
 if __name__ == '__main__':
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
+
 def create_app():
+    #run 'app' as a Flask app
     app = Flask(__name__)
+    #Cookie encryption key
     app.config['SECRET_KEY'] = 'OG57vlTo0k'
 
+    #run flask app
     return app
