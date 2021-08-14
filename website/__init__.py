c2f42cc07eed83763e6de36c62996afd91a6e6ec
SCJackB <broughjv@scotscollege.school.nz>
2021-08-14 16:11:52 +1200

    Added funtionallity to email form

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 
 def create_app():
     #define 'app' as a Flask app
     app = Flask(__name__)
     #Cookie encryption key
-    app.config['SECRET_KEY'] = 'OG57vlTo0k'
+    app.config['SECRET_KEY'] = os.urandom(12).hex()
 
     #import the views blueprint from the 'website' package
     from .views import views
